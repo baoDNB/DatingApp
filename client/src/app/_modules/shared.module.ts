3d1cd4473cee60affebbfb589d7ca8e5d548f64e
baoDNB <baopa0805@gmail.com>
2023-03-07 14:00:32 +0700

    End of section 7

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -9,7 +9,7 @@ import { ToastrModule } from 'ngx-toastr';
     CommonModule,
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
-      positionClass:'toastr-bottom-right'
+      positionClass:'toast-bottom-right'
     })
   ],
   exports:[
