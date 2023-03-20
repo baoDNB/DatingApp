f988ac27180264507443d74f09214d6f1b197a93
baoDNB <baopa0805@gmail.com>
2023-03-20 10:38:45 +0700

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace API.Controllers
@@ -41,6 +42,21 @@ namespace API.Controllers
         {
             return await _userRepository.GetMemberAsync(username);
             
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+           var username= User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+           var user =await _userRepository.GetUserByUsernameAsync(username);
+           
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+
         }
     }
 }
